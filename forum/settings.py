c052490de5ed49be448b34a132e28b26d9739e85
Mohammad Samim Noori <mohammadsamimnoori@Mohammads-MacBook-Air.local>
2021-10-26 16:26:20 -0700

    last commit

--- forum/settings.py
+++ forum/settings.py
@@ -124,21 +124,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 cloudinary.config(
     cloud_name='moha-noori',
@@ -147,7 +133,12 @@ cloudinary.config(
     secure=True
 )
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
